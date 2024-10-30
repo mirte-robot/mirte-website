f6a78a345802ef95850b1183388f78851366471c
Martin Klomp <m.klomp@tudelft.nl>
2024-10-30 12:00:52 +0100

    Using VueJS router props

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,45 +2,42 @@
 
 
   <div class="container-fluid">
-    <a class="navbar-brand" style="font-family: Livvic-Bold;" href="/"><h1>MIRTE</h1></a>
+    <router-link :to="{name: 'home'}" class="navbar-brand" style="font-family: Livvic-Bold;"><h1>MIRTE</h1></router-link>
     <button class="navbar-toggler" type="button" @click="visible=!visible" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="navbar-collapse" :class="!visible?'collapse':''" id="navbarNavDropdown">
       <ul class="navbar-nav ms-auto" >
         <li class="nav-item">
-          <router-link to="/" class="nav-link" @click="visible=!visible">{{ $t("navbar.home") }}</router-link>
+          <router-link :to="{name: 'home'}" class="nav-link" @click="visible=!visible">{{ $t("navbar.home") }}</router-link>
         </li>
-<!--        <li class="nav-item">
-          <router-link to="/whois" class="nav-link">{{ $t("navbar.whois") }}</router-link>
-        </li>  -->
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="/#/robots" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <a class="nav-link dropdown-toggle" href="/robots" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             {{ $t("navbar.robots") }}
           </a>
           <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuLink">
-            <router-link to="/robots?r=light" class="nav-link" @click="visible=!visible"><span class="mirte">MIRTE</span> Light</router-link>
-            <router-link to="/robots?r=basic" class="nav-link" @click="visible=!visible"><span class="mirte">MIRTE</span> Basic</router-link>
-            <router-link to="/robots?r=pioneer" class="nav-link" @click="visible=!visible"><span class="mirte">MIRTE</span> Pioneer</router-link>
+            <router-link :to="{name: 'robots', params: { robot: 'light' }}" class="nav-link" @click="visible=!visible"><span class="mirte">MIRTE</span> Light</router-link>
+            <router-link :to="{name: 'robots', params: { robot: 'basic' }}" class="nav-link" @click="visible=!visible"><span class="mirte">MIRTE</span> Basic</router-link>
+            <router-link :to="{name: 'robots', params: { robot: 'pioneer' }}" class="nav-link" @click="visible=!visible"><span class="mirte">MIRTE</span> Pioneer</router-link>
             <div class="dropdown-divider"></div>
-            <router-link to="/configure?r=pioneer&c=true" class="nav-link" @click="visible=!visible">{{ $t("navbar.configure") }}</router-link>
+            <router-link :to="{name: 'configure', params: { robot: 'pioneer', selectable: true }}" class="nav-link" @click="visible=!visible">{{ $t("navbar.configure") }}</router-link>
           </ul>
         </li>
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="/#/robots" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <a class="nav-link dropdown-toggle" href="/start" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             {{ $t("navbar.start") }}
           </a>
           <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuLink">
-            <router-link to="start?l=primary" class="nav-link" @click="visible=!visible">{{ $t("navbar.primary") }}</router-link>
-            <router-link to="start?l=secondary" class="nav-link" @click="visible=!visible">{{ $t("navbar.secondary") }}</router-link>
-            <router-link to="start?l=higher" class="nav-link" @click="visible=!visible">{{ $t("navbar.higher") }}</router-link>
+            <router-link :to="{name: 'start', params: { level: 'primary' }}" class="nav-link" @click="visible=!visible">{{ $t("navbar.primary") }}</router-link>
+            <router-link :to="{name: 'start', params: { level: 'secondary' }}" class="nav-link" @click="visible=!visible">{{ $t("navbar.secondary") }}</router-link>
+            <router-link :to="{name: 'start', params: { level: 'higher' }}" class="nav-link" @click="visible=!visible">{{ $t("navbar.higher") }}</router-link>
             <div class="dropdown-divider"></div>
             <a :href="'https://workshops.mirte.org/' + $i18n.locale" class="nav-link">{{ $t("navbar.workshops") }}</a>
             <a href="https://docs.mirte.org/" class="nav-link">{{ $t("navbar.docs") }}</a>
           </ul>
         </li>
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="/#/robots" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <a class="nav-link dropdown-toggle" href="/contribute" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             {{ $t("navbar.contribute") }}
           </a>
           <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuLink">
--- src/router/index.js
+++ src/router/index.js
@@ -1,36 +1,43 @@
 import { createWebHistory, createRouter } from "vue-router";
+
 import Home from "@/views/Home.vue";
 import Robots from "@/views/Robots.vue";
 import Configure from "@/views/Configure.vue";
 import Start from "@/views/Start.vue";
 
 const routes = [
-  {
-    path: "/",
-    name: "Home",
+  { path: "/",
     component: Home,
+    name: 'home'
   },
-  {
-    path: "/robots",
-    name: "Robots",
+  { path: "/robots",
+    name: 'robots',
     component: Robots,
+      props: (route) => ({
+        robot: route.params.robot
+      })
   },
-  {
-    path: "/configure",
-    name: "Configure",
+  { path: "/configure",
+    name: 'configure',
     component: Configure,
+      props: (route) => ({
+        robot: route.params.robot,
+        selectable: route.params.selectable
+      })
   },
-  {
-    path: "/start",
-    name: "Start",
+  { path: "/start",
+    name: 'start',
     component: Start,
+      props: (route) => ({
+        level: route.params.level,
+        robot: route.params.robot
+      })
   }
 ];
 
 const router = createRouter({
   history: createWebHistory(),
-  base: '/web_new/',
-  routes,
+  routes
 });
 
 export default router;
